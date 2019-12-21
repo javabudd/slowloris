25be9885a8f69b5a5d3755fef4939ca31d1235e2
Andy OBrien <javabudd@gmail.com>
2019-12-20 20:15:16 -0700

    Remove composer check

--- slowloris.php
+++ slowloris.php
@@ -1,11 +1,5 @@
 <?php
 
-if (!\file_exists(__DIR__ . '/vendor/autoload.php')) {
-    exit('Missing autoload file, have you ran composer install?');
-}
-
-require_once __DIR__ . '/vendor/autoload.php';
-
 const USER_AGENTS = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
